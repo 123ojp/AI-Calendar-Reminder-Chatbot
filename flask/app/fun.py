e6faab174479e42e259025e5790db62db9a07bf4
o123ojp <shepherd123ojp@gmail.com>
2018-12-02 01:36:03 +0800

    change

--- flask/app/fun.py
+++ flask/app/fun.py
@@ -25,10 +25,10 @@ class Fun():
             time_re = re.search('([0-9]{1,2})[^\d]*([0-9]{1,2})',time)
             hour = int(time_re.group(1))
             min = int(time_re.group(2))
-            date_re = re.search('([0-9]{4})-([0-9]{2})-([0-9]{2})',date)
-            year = int(date_re.group(1))
-            month = int(date_re.group(2))
-            day = int(date_re.group(3))
+            date_re = re.search('([0-9]{1,2})[^\d]*([0-9]{1,2})',date)
+            year = 2018
+            month = int(date_re.group(1))
+            day = int(date_re.group(2))
         unix_time = datetime.datetime(year,month,day,hour,min).timestamp()
         date = str(year)+"年"+str(month)+"月"+str(day)+"日"
         time = str(hour)+":"+str(min)
