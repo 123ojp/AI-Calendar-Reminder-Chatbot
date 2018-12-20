88a5c48c857b985d3edfef459cfb647d93bee6e2
童慰國 <32390039+rabbit1112@users.noreply.github.com>
2018-12-20 14:20:03 +0800

    Update fun.py
    getActDate

--- flask/app/fun.py
+++ flask/app/fun.py
@@ -40,3 +40,38 @@ class Fun():
         if room_type == "user":
             id = org_req.get('payload').get('data').get('source').get('userId')
         return id
+    def getActDate(self,parameters,org_req):
+        try:
+            date = parameters.get('date')
+            try:
+                date_re = re.search('([0-9]{4})-([0-9]{2})-([0-9]{2})',date)
+                year = int(date_re.group(1))
+                month = int(date_re.group(2))
+                day = int(date_re.group(3))
+                date = str(year)+"年"+str(month)+"月"+str(day)+"日"
+                return date,1
+            except:
+                date_re = re.search('([0-9]{4})-([0-9]{2})',date)
+                year = int(date_re.group(1))
+                month = int(date_re.group(2))
+                date = str(year)+"年"+str(month)+"月"
+                return date,2
+        except:
+            text = org_req.get('payload').get('data').get('message').get('text')
+            text = text.split(" ")
+            if(len(text) == 1):
+                return 0,0
+            date = text[1]
+            try:
+                date_re = re.search('([0-9]{1,2})[^\d]([0-9]{1,2})',date)             
+                year = 2018
+                month = int(date_re.group(1))
+                day = int(date_re.group(2))
+                date = str(year)+"年"+str(month)+"月"+str(day)+"日"
+                return date,1
+            except:
+                date_re = re.search('([0-9]{1,2})[^\d]',date)            
+                year = 2018
+                month = int(date_re.group(1))
+                date = str(year)+"年"+str(month)+"月"
+                return date,2
