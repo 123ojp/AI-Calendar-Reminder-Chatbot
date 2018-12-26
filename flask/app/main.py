5cb5c0e15ad299820fcf056258a7161704203028
machine116688 <32387410+machine116688@users.noreply.github.com>
2018-12-26 11:33:16 +0800

    fix last no \n

--- flask/app/main.py
+++ flask/app/main.py
@@ -182,7 +182,9 @@ def webhook():
             respone_text += "欲刪除請使用'確認刪除'加上空格及欲刪除編號'\n"
             respone_text += "例子:確認刪除 6\n"
             for a in range(0, len(display_act)):
-                respone_text += str(a+1) + " =>" + display_act[a] + "\n"
+                respone_text += str(a+1) + " =>" + display_act[a] #(a+1)是為因user習慣
+            if( a != len(display_act) ): #最後一個不要跳行
+                respone_text += '\n'
         print("執行delAct")
     
     #
