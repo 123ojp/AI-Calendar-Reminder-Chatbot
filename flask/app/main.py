ce2444535245e9894ab5dfa5f0f86ea0bb767433
machine116688 <32387410+machine116688@users.noreply.github.com>
2018-12-27 20:23:42 +0800

    \n

--- flask/app/main.py
+++ flask/app/main.py
@@ -212,7 +212,7 @@ def webhook():
             respone_text = "刪除成功"
         if (suc_message == "false"):
             print("刪除失敗，請重新執行'刪除'指令/n並且確定有此編號")
-            respone_text = "刪除失敗，請重新執行'刪除確認'指令/n並且確定有此編號"
+            respone_text = "刪除失敗，請重新執行'刪除確認'指令\n並且確定有此編號"
         else:
             print('error in return')
     
